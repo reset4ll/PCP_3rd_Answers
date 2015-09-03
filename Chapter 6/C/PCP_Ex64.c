06d5015718af0655ea5a4ba039cbe1920f8828d1
reset4ll <reset4ll@yahoo.es>
2015-09-03 19:32:30 +0200

    Subido PCP_Ex64

---
+++ Chapter 6/C/PCP_Ex64.c
@@ -0,0 +1,76 @@
+/* Exercise 6.4 of Practical C Programming, 3rd Ed. */
+/* Desglosa una cantidad de dinero introducida menor de 1$*/
+/* A.Esteban, 2015 */
+
+#include <stdio.h>
+
+//* Números mágicos
+#define QUARTER 25
+#define DIME 10
+#define NICKEL 5
+#define PENNY 1
+
+int cantidad = 0, tquarters = 0, tdimes = 0, tnickels = 0;
+int tpenniesQ = 0, tpenniesD = 0, tpenniesN = 0;
+
+int main(int argc, char const *argv[])
+{
+
+	printf("Introduce la cantidad de dinero: ");
+	scanf("%d", &cantidad);
+
+	while (cantidad >= 0 && cantidad <= 101) { 	// *Loop
+
+		{	//* Quarters
+			if (cantidad >= QUARTER) 
+
+				tquarters = cantidad / QUARTER;
+				tpenniesQ = cantidad % 25;
+				tdimes = cantidad / DIME;
+				tpenniesD = cantidad % 10;
+				tnickels = cantidad / NICKEL;			
+				tpenniesN = cantidad % 5;
+
+			printf("\nDesglose de la cantidad introducida:\n");
+			printf("\n\tQuarters = %d y Pennies = %d\n", tquarters, tpenniesQ);
+			printf("\tDimes = %d y Pennies = %d\n", tdimes, tpenniesD);
+			printf("\tNickels = %d y Pennies = %d\n", tnickels, tpenniesN);
+			printf("\tPennies = %d\n", cantidad);
+
+			return 0;
+		}
+
+
+		{	//* Dimes
+			if (cantidad >= DIME)  
+				tdimes = cantidad / DIME;
+				tpenniesD = cantidad % 10;
+				tnickels = cantidad / NICKEL;			
+				tpenniesN = cantidad % 5;
+
+			printf("\nDesglose de la cantidad introducida:\n");	
+			printf("\n\tDimes = %d y Pennies = %d\n", tdimes, tpenniesD);
+			printf("\tNickels = %d y Pennies = %d\n", tnickels, tpenniesN);
+			
+			return 0;
+		}
+
+
+		{	//* Nickels
+			if (cantidad >= NICKEL)
+				tnickels = cantidad / NICKEL;			
+				tpenniesN = cantidad % 5;
+
+			printf("\nDesglose de la cantidad introducida:\n");	
+			printf("\n\tNickels = %d y Pennies = %d\n", tnickels, tpenniesN);
+
+			return 0;
+			
+		}
+
+		return 0;
+	}
+
+	printf("Error, prueba otra vez!");  //* Error.
+	return -1;
+}
---
+++ Chapter 6/CPP/PCP_Ex64_plus.cpp
@@ -0,0 +1,81 @@
+//* Exercise 6.4 of Practical C Programming, 3rd Ed. */
+//* Desglosa una cantidad de dinero introducida menor de 1$*/
+//* A.Esteban, 2015 */
+
+#include <iostream>
+
+using namespace std;
+
+//* Números mágicos
+#define QUARTER 25
+#define DIME 10
+#define NICKEL 5
+#define PENNY 1
+
+int cantidad = 0, tquarters = 0, tdimes = 0, tnickels = 0;
+int tpenniesQ = 0, tpenniesD = 0, tpenniesN = 0;
+
+int main(int argc, char const *argv[])
+{
+
+	cout <<"Introduce la cantidad de dinero: ";
+	cin >> cantidad;
+
+	while (cantidad >= 0 && cantidad <= 101) { 	// *Loop
+
+		{	//* Quarters
+			if (cantidad >= QUARTER) 
+
+				tquarters = cantidad / QUARTER;
+				tpenniesQ = cantidad % 25;
+				tdimes = cantidad / DIME;
+				tpenniesD = cantidad % 10;
+				tnickels = cantidad / NICKEL;			
+				tpenniesN = cantidad % 5;
+
+			cout << endl << "Desglose de la cantidad introducida: " << endl; 
+			cout << endl;
+			cout << "Quarters = " << tquarters << " y Pennies = " << tpenniesQ << endl;
+			cout << "Dimes = " << tdimes << " y Pennies = " << tpenniesD << endl;
+			cout << "Nickels = " << tnickels << " y Pennies = " << tpenniesN << endl;
+			cout << "Pennies = " << cantidad << endl;
+
+			return 0;
+		}
+
+
+		{	//* Dimes
+			if (cantidad >= DIME)  
+				tdimes = cantidad / DIME;
+				tpenniesD = cantidad % 10;
+				tnickels = cantidad / NICKEL;			
+				tpenniesN = cantidad % 5;
+
+			cout << endl << "Desglose de la cantidad introducida: " << endl;
+			cout << endl;	
+			cout << "Dimes = " << tdimes << " y Pennies = "  << tpenniesD << endl;
+			cout << "Nickels = " << tnickels << " y Pennies = " << tpenniesN << endl;
+			
+			return 0;
+		}
+
+
+		{	//* Nickels
+			if (cantidad >= NICKEL)
+				tnickels = cantidad / NICKEL;			
+				tpenniesN = cantidad % 5;
+
+			cout << endl << "Desglose de la cantidad introducida: " << endl;
+			cout << endl;	
+			cout << "Nickels = " << tnickels << " y Pennies = " << tpenniesN << endl;
+
+			return 0;
+			
+		}
+
+		return 0;
+	}
+
+	cout <<"Error, prueba otra vez!";  //* Error.
+	return -1;
+}
